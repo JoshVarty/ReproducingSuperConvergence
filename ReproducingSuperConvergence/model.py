94c3a6b572246fc78675ef64f6d684787a8ce721
Josh Varty <joshvarty@gmail.com>
2017-11-16 19:58:46 -0500

    Paper's dimensions don't make sense for this layer. Must be stride 1

--- ReproducingSuperConvergence/model.py
+++ ReproducingSuperConvergence/model.py
@@ -121,7 +121,7 @@ def TrainModel(lr = 0.001):
         shape = input.shape.as_list()
         weights = weight_layer("w_conv1", [3, 3, shape[3], 16])
         bias = bias_variable("b_conv1", [16])
-        net = tf.nn.conv2d(input, weights, strides=[1,2,2,1], padding='SAME') + bias
+        net = tf.nn.conv2d(input, weights, strides=[1,1,1,1], padding='SAME') + bias
         net = tf.layers.batch_normalization(net, name="bn_conv1", momentum=0.95)
         net = tf.nn.relu(net)
         
